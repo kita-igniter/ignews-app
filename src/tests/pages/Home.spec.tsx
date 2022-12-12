94c95329a1bd451a2405176956c8bdce6a2bf94b
Kita Igniter <smtecsystem.469@gmail.com>
2022-12-12 07:17:51 -0300

    Testing GetStaticProps

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import { stripe } from "../../services/stripe";
 import { useSession } from "next-auth/react";
-import Home from "../../pages";
+import Home, { getStaticProps } from "../../pages";
 
 jest.mock("next/router");
 jest.mock("next-auth/react");
+jest.mock("../../services/stripe");
 
 describe("Home Page", () => {
   const useSessionMocked = jest.mocked(useSession); 
@@ -13,5 +15,28 @@ describe("Home Page", () => {
     render(<Home product={{ priceId: "fake-price-id", amount: "R$10,00"}} />);
 
     expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+
+  it("loads initial data", async () => {
+    const stripePricesRetrieveMocked = jest.mocked(stripe.prices.retrieve);
+
+    stripePricesRetrieveMocked.mockResolvedValueOnce({
+      id: "fake-prices-id",
+      unit_amount: 1000,
+    } as any);
+
+    const res = await getStaticProps({});
+
+    expect(res).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-prices-id",
+            amount: "$10.00"
+          }
+        },
+        revalidate: 86400,
+      })
+    );
   });
 });
